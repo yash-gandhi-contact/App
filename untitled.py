a5521f6fcfd5995d052010293980b728cda0abee
Yash Gandhi <152842562+yash-gandhi-contact@users.noreply.github.com>
2024-08-07 12:01:29 +0200

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -180,8 +180,8 @@ def main():
     # Add your name and LinkedIn logo below the main logo
     linkedin_logo_url = "https://cdn-icons-png.flaticon.com/512/174/174857.png"
     st.sidebar.markdown(f"""
-        <div style='text-align: left; margin-top: 6px;'>
-            <small>Made by Yash Gandhi</small><br>
+        <div style='text-align: center; margin-top: 10px;'>
+            <small>by Yash Gandhi</small><br>
             <a href='https://www.linkedin.com/in/yash--gandhi/' target='_blank'>
                 <img src='{linkedin_logo_url}' alt='LinkedIn Profile' style='width: 20px; height: 20px;'>
             </a>
@@ -190,7 +190,11 @@ def main():
 
     # Sidebar navigation for multiple pages
     st.sidebar.title("Navigation")
-    page_selection = st.sidebar.radio("Go to", ["Dynamic Excel Data Query", "Update Entries"])
+    page_selection = st.sidebar.selectbox(
+        "Select Page",
+        ["Update Entries", "Dynamic Excel Data Query"],
+        index=0  # Set "Update Entries" as the default
+    )
 
     # Render the selected page
     if page_selection == "Dynamic Excel Data Query":
